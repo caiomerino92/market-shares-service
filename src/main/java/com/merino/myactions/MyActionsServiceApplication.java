d2949276ab75195bc7e8fe49a313e5dd0687248f
caio.merino <caio.merino@dextra-sw.com>
2020-09-13 09:03:20 -0300

    First commit

---
+++ src/main/java/com/merino/myactions/MyActionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.merino.myactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyActionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyActionsServiceApplication.class, args);
+	}
+
+}
